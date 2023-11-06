4225bf536907b014cd37a074c294943262d414a8
Kivtas <satviktiwarikivtas7@gmail.com>
2023-11-06 17:52:26 +0000

    made changes

--- src/Face2FaceIndex.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include "Vertex.h"
-#include "FileHandler.h"
-#include <string>
--- src/FaceIndex2DirectedEdge.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include "FileHandler.h"
-#include "OtherHalf.h"
-#include "FirstDirectedEdge.h"
-#include "Genus.h"
